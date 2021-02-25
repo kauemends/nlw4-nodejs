bc64018e9584dc944e3f7514bc9a5b3041353ee3
Kauê Mendes <kmendesrodrigues@gmail.com>
2021-02-25 12:20:40 -0300

    thursday

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { SendMailControlller } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
 
@@ -7,9 +8,13 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 
+const sendMailController = new SendMailControlller();
+
 router.post("/users", userController.create);
 
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
